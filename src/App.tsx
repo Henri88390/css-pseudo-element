674e7424f1ee6a0eb80117c42a803c3a9f646dc8
Henri88390 <henri.lambert9@gmail.com>
2023-11-01 08:21:00 +0100

    feat: add a required symbol with the ::after pseudo element

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="form">
+      <span className="input">
+        <label className="required">Name</label>
+        <input type="text" required />
+      </span>
+      <button data-tooltip="Tooltip">Submit form</button>
+    </div>
+  );
 };
 
 export default App;
